a31b54a5bd5fd3f0b4fda00dd48a7589611681df
Ahad Zai <zaia@berea.edu>
2020-02-14 14:53:55 -0500

    started working on the home app

--- TracyTrainingModules/TracyTrainingModules/urls.py
+++ TracyTrainingModules/TracyTrainingModules/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home_app import views as h_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',h_view.home,name='home'),
 ]
