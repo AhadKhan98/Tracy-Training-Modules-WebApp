697936da643ed78ad72ff580a0ce0ceb56de0288
AhadKhan98 <ahadkhan98@hotmail.com>
2020-04-03 07:15:48 -0400

    login page checks for valid/invalid access_code.

--- TracyTrainingModules/home_app/views.py
+++ TracyTrainingModules/home_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from home_app.models import Custom_User
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        post_data = dict(request.POST)
+        access_code = post_data['access_code'][0]
+        print('ACCESS CODE: ' + access_code)
+        return render(request,'home.html',{access_code:access_code})
     return render(request,'home.html')
 
 def login(request):
-    return render(request,'login.html')
+    all_users = Custom_User.objects.all()
+    all_access_codes = []
+    for user in all_users:
+        all_access_codes += [user.access_code]
+    return render(request,'login.html',{'all_access_codes':all_access_codes})
 
 def register(request):
     return render(request,'register.html')
