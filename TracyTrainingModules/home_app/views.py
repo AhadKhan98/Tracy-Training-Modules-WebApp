f21c3f3bfa9cab2d7c804e404e509da424f54fb2
AhadKhan98 <ahadkhan98@hotmail.com>
2020-04-06 10:27:45 -0400

    added database functionality on login and register pages. Need to work on cookies to save logged in user.

--- TracyTrainingModules/home_app/views.py
+++ TracyTrainingModules/home_app/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from home_app.models import Custom_User
+import random,string
 
 # Create your views here.
 def home(request):
+    all_users = Custom_User.objects.all()
+    all_access_codes = []
+    for user in all_users:
+        all_access_codes += [user.access_code]
+
+
+    def generate_access_code():
+        # Getting all previously stored access codes from the database.
+        all_characters = string.ascii_letters + string.digits
+        access_code = "".join([random.choice(all_characters) for x in range(25)])
+        if access_code in all_access_codes:
+            generate_access_code()
+        return access_code
+
     if request.method == 'POST':
-        post_data = dict(request.POST)
-        access_code = post_data['access_code'][0]
-        print('ACCESS CODE: ' + access_code)
-        return render(request,'home.html',{access_code:access_code})
+        if 'access_code' in request.POST: # Coming from login.html
+            access_code = request.POST['access_code']
+            user_fname = Custom_User.objects.get(access_code=access_code).first_name
+            return render(request,'home.html',{'access_code':access_code,'user_fname':user_fname})
+        elif 'first_name' in request.POST: # Coming from register.html
+            first_name,last_name,email = request.POST['first_name'],request.POST['last_name'],request.POST['email']
+            access_code = generate_access_code()
+
+            user = Custom_User(access_code=access_code,user_type='sup',first_name=first_name,last_name=last_name,email=email,sections='1,0,0,0,0,0,0,0,0,0,0')
+            user.save()
+            print('saved to database')
     return render(request,'home.html')
 
 def login(request):
